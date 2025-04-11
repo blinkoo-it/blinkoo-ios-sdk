4b3833faeeb673b66a3cf1a053a188b38c935155
Davide Quadrelli <ildado94@gmail.com>
2025-04-11 14:00:21 +0100

    Release 0.0.4

--- Package.swift
+++ Package.swift
@@ -33,43 +33,43 @@ let package = Package(
         ),
         .binaryTarget(
             name: "App",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/App.xcframework.zip",
-            checksum: "422326c32ec733679c86ba66b444eee075352065505d7049404acbd4c93f6557"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/App.xcframework.zip",
+            checksum: "32624b47b721b3af5326eb11c25e6d630eb99ef668c1527cdd6dc1a3a0dd6b16"
         ),
 .binaryTarget(
             name: "shared_preferences_foundation",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/shared_preferences_foundation.xcframework.zip",
-            checksum: "41724032a040a1cb28ddc36bb4a56ba96477fa3c823e290008c6610f3e664fb6"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/shared_preferences_foundation.xcframework.zip",
+            checksum: "80fe4071a0ce17f9989df1611a10768ee458c747ec5f6f94bf4faba4a3cb26b3"
         ),
 .binaryTarget(
             name: "package_info_plus",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/package_info_plus.xcframework.zip",
-            checksum: "5183b6ea97e09575971e604b1557e469ca7141e061262e551ccf2c3f76eb2274"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/package_info_plus.xcframework.zip",
+            checksum: "c4a7ee38fea38c04dda97764dbc1f476a33566a2069da9b7ab2122e7535476fe"
         ),
 .binaryTarget(
             name: "FlutterPluginRegistrant",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/FlutterPluginRegistrant.xcframework.zip",
-            checksum: "736517dcfff09e7cfff1be837ef11c6f6e822af950abf9a08bbc43073bac32f8"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/FlutterPluginRegistrant.xcframework.zip",
+            checksum: "27423227a820f8b7e95a071e465e5cd2d700d6225c4005ea89144fd0f144145a"
         ),
 .binaryTarget(
             name: "video_player_avfoundation",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/video_player_avfoundation.xcframework.zip",
-            checksum: "ddcde0d9f32c2f22f89d06964c9dc6a545aba1d22a59b3cdc4f9362d62bc3ab9"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/video_player_avfoundation.xcframework.zip",
+            checksum: "9c9f684837726b72097b68bff87d693d0df36970a2e79178ab4c4d1251782d0f"
         ),
 .binaryTarget(
             name: "path_provider_foundation",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/path_provider_foundation.xcframework.zip",
-            checksum: "979458391d71187780c032f441a83084ffeb7e6ad9df79bd6e133956207eb395"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/path_provider_foundation.xcframework.zip",
+            checksum: "af87deb8389f93cf4df2bf990bb51d5b69b662f6783c47ccee62fb3764b34075"
         ),
 .binaryTarget(
             name: "share_plus",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/share_plus.xcframework.zip",
-            checksum: "4e4dc70d2452aa2e107285372f01030192cc7339327b32589589b68e1677031c"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/share_plus.xcframework.zip",
+            checksum: "921b50409f618e9093415b65be127d4b50438a8978dc5810bf35cc668b332d6a"
         ),
 .binaryTarget(
             name: "Flutter",
-            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.3/Flutter.xcframework.zip",
-            checksum: "1362790bec13b78de5e8976ccc862e4276db8dd111fb2ab4917302ff6e33ac75"
+            url: "https://maven-repository.blinkoo.com/repository/ios-sdk/0.0.4/Flutter.xcframework.zip",
+            checksum: "13bf76c29fb5556e8c85755c934ea278aba6bda1a10aecb5e6fbe64a428d36a9"
         )
         ]
 )
